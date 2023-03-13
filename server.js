aa68b1f2922354dbdf1217133c9db695729f29a1
MightiestAlex <alx@hotmail.co.uk>
2023-03-13 23:44:12 +0000

    CORS

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const cors = require('cors');
 const apiRouter = require('./routes/apirouter.js');
 
 const {handleStatus400, handleStatus500, handlePSQLerrors} = require('./controllers/errorhandlers.js');
 const express = require('express');
 
+
 const app  = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
